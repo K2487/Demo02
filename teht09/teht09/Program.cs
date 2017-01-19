394c822b4e2a1076ce090022bab2f49853cbe21e
K2487 <K2487@student.jamk.fi>
2017-01-19 15:07:36 +0200

    teht09

--- teht09/teht09/Program.cs
+++ teht09/teht09/Program.cs
@@ -10,6 +10,34 @@ namespace teht09
     {
         static void Main(string[] args)
         {
+            int luku0;
+            int luku1;
+
+
+            Console.WriteLine("Anna luku >");
+            string line = Console.ReadLine();
+            luku1 = int.Parse(line);
+            luku0 = luku1;
+            if (luku1 == 0)
+            {
+                Console.WriteLine("Lukujen summa on " + luku0);
+            }
+            while (luku1 != 0)
+            {
+
+                Console.WriteLine("Anna luku >");
+                line = Console.ReadLine();
+                luku1 = int.Parse(line);
+                luku0 = luku1;
+                if (luku1 == 0)
+                {
+                    Console.WriteLine("Lukujen summa on " + luku0);
+                }
+            }
         }
     }
 }
+
+    
+        
+    
